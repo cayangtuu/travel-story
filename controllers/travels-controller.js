137628689144ef6f0567627f817b92efd8103b18
cayang <cayang@simenvi.com.tw>
2023-03-13 16:33:45 +0800

    feat: add create new travel page

--- controllers/travels-controller.js
+++ controllers/travels-controller.js
@@ -1,6 +1,9 @@
 const travelController = {
   getTravels: (req, res, next) => {
-    return res.render('travels')
+    return res.render('travels', { travels })
+  },
+  createTravel: (req, res, next) => {
+    return res.render('create-travel')
   }
 }
 module.exports = travelController
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const travelController = require('../controllers/travels-controller')
 
+router.get('/create', travelController.createTravel)
 router.get('/', travelController.getTravels)
 
 module.exports = router
